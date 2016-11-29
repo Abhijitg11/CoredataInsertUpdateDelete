14e0fda145ec9022921ef1736f31b77b43ab1900
Student <Student@apples-MacBook-Air-3.local>
2016-11-29 09:10:29 +0530

    Initial Commit

---
+++ machinetestInserUpdatedeleteCoredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  machinetestInserUpdatedeleteCoredata
+//
+//  Created by Student on 29/11/16.
+//  Copyright (c) 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
